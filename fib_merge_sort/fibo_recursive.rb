50d7b22f029c1f20ffd1ee79907a30fc185ab2e5
Miguel Figuera <miguelqui725@gmail.com>
2022-06-26 18:40:57 -0400

    result

--- fib_merge_sort/fibo_recursive.rb
+++ fib_merge_sort/fibo_recursive.rb
@@ -1,26 +1,23 @@
 def fib_recursive(num)
-    num< 2 ? num : fib_recursive(num -1) + fib_recursive(num-2)
+  num < 2 ? num : fib_recursive(num - 1) + fib_recursive(num - 2)
 end
 
+def fib_iterative(num, array = [0, 1])
+  if num < 2
+    array[num]
+  else
+    array.push(array[-1] + array[-2]) until num == array.length
+  end
+  array
+end
 
-def fib_iterative(num,array=[0,1])
-    if num < 2
-        array[num]
-    else
-        until num == array.length
-            array.push(array[-1] + array[-2])
-        end
-    end
+def fib_rec3(num, array = [0, 1])
+  if num == array.length
     array
-end
-
-def fib_rec3(num,array=[0,1])
-    if num == array.length
-        array 
-    else
+  else
     array.push(array[-1] + array[-2])
-    fib_rec3(num,array)
-    end
+    fib_rec3(num, array)
+  end
 end
 
-#all of this work
+# all of this work
--- fib_merge_sort/mergesort_recursive.rb
+++ fib_merge_sort/mergesort_recursive.rb
@@ -1,16 +1,23 @@
-to_sort = Array.new(10) {rand(1..100)}
+require 'pry'
 
+to_sort = Array.new(10) { rand(1..100) }
 
 def merge_sort(array)
-    
-    #Base case
-    if array.length < 2
-        array
-    end
+  # Base case
+  binding.pry
+  return array if array.length < 2
 
-    mid = array.length / 2
+  mid = array.length / 2
 
+  left = merge_sort(array[0...mid])
+  right = merge_sort(array[mid + 1...-1])
 
-    #divide an array until length == 1
-    #compare the result of the result and push the lesser value
-    #
+  # Sorting and merging
+  result = []
+  left.first < right.first ? result.push(left.first) : result.push(right.first) until left.empty? || right.empty?
+
+  good_result = result.uniq!
+  p good_result
+end
+
+merge_sort(to_sort)
